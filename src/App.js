d37c92d9f0bbd069b60934b325f7d2d34f2a8263
francoromerobrewer <francoromerobrewer12@gmail.com>
2021-09-07 16:02:32 +0300

    app uploaded to git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Phone from './components/svg-animation/Phone'
+import Gym from './components/svg-animation/Gym'
+import Sushi from './components/svg-animation/Sushi';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+            <Route exact path="/">
+              <Sushi />
+            </Route>
+            <Route path="/phone">
+              <Phone />
+            </Route>
+            <Route path="/gym">
+              <Gym />
+            </Route>
+          </Switch>
+      </Router>
     </div>
   );
 }
